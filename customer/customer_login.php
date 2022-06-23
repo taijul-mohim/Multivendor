e1eec698342be071d3bc13e0d0bc27e45aa0815a
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2022-06-23 20:27:10 +0600

    register

---
+++ customer/customer_login.php
@@ -0,0 +1,31 @@
+<div class="box">
+    <div class="box-header">
+        <center>
+            <h2>Login</h2>
+        </center>
+        <p class="lead"> Already our customer </p>
+    </div>
+    <form action="" method="">
+        <div class="form-grop">
+            <label >Email:</label>
+            <input type="text" class="form-control" name="c_email" required=''>
+
+        </div>
+       <div class="form-grop">
+            <label >Email:</label>
+            <input type="password" class="form-control" name="c_pass" required=''>
+            
+        </div>
+    <div class="text-center">
+        <button name="login" value="login" class="btn btn-primary">
+            <i class="fa fa-sign-in"></i> Login
+        </button>
+    </div>
+
+    </form>
+    <center>
+        <a href="customer_register.php">
+            <h3>New? Register Now</h3>
+        </a>
+    </center>
+</div>
