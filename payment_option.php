78bafda43bd7257d3f34003d21a16aa6e7ab6828
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2022-06-26 18:20:49 +0600

    order  id  create

--- payment_option.php
+++ payment_option.php
@@ -1,7 +1,26 @@
 <div class="box">
+
+<?php
+ $session_email=$_SESSION['customer_email'];
+ $select_customer="SELECT * FROM customer WHERE c_email='$session_email'";
+$run_cust=mysqli_query($con,$select_customer);
+$row_customer=mysqli_fetch_array($run_cust);
+$customer=$row_customer['customer_id'];
+
+
+
+
+
+?>
+
+
+
+
+
+
     <h1 class="text-center">Payment options</h1>
     <p class="lead text-center">
-        <a href="">Pay offline</a>
+        <a href="order.php?<?php echo $customer ?>">Pay offline</a>
     </p>
   <center>
     <p class="lead">
